897dd9f19a47d1edb5162bf40628ba05ea5d8eb4
phhephhe <beridzedavit07@gmail.com>
2023-01-03 01:34:35 +0400

    added close menu functional on click somewhere except menu

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const closeBtn = document.getElementById('closeBtn');
 const menuBtn = document.getElementById('menuBtn');
 const menu = document.querySelector('.menu');
+const blur = document.querySelector('.blur');
+const body = document.querySelector('body');
 
-console.log(menu)
 menuBtn.addEventListener('click',(el) => {
     el.preventDefault()
     menu.classList.add('active')
+    blur.style.display = 'block'
 })
 
 closeBtn.addEventListener('click', () => {
     menu.classList.remove('active')
+    blur.style.display = 'none'
+})
+body.addEventListener('click', (event) => {
+    if(!menuBtn.contains(event.target) && !menu.contains(event.target)){
+        menu.classList.remove('active')
+        blur.style.display = 'none'
+    }
 })
